69096c8f373bf808766ba215c01029078abdd08b
Den Raskovalov <denbox@gmail.com>
2015-05-07 01:28:35 -0700

    gen int

--- 1369/1369.cpp
+++ 1369/1369.cpp
@@ -299,6 +299,164 @@ struct KDTree
     }
 };
 
+struct CircleTree
+{
+    int _len;
+    TVector* _x;
+    TVector* _y;
+    bool _isLeaf;
+    CircleTree* _inner;
+    CircleTree* _outer;
+    TIntVector _indices;
+    double _cx;
+    double _cy;
+    double _maxRad;
+    double _rad;
+
+    CircleTree(int depth, const TDoubles& x, const TDoubles& y, const TIntVector& indices)
+        : _inner(nullptr)
+        , _outer(nullptr)
+    {
+        _cx = 0;
+        _cy = 0;
+        for (size_t i = 0; i < x.size(); ++i)
+        {
+            _cx += x[i];
+            _cy += y[i];
+        }
+        _cx /= x.size();
+        _cy /= x.size();
+        _maxRad = 0;
+        TDoubles rads(x.size());
+        for (size_t i = 0; i < x.size(); ++i)
+        {
+            double rad = sqrt( Sqr(x[i] - _cx) + Sqr(y[i] - _cy) );
+            _maxRad = Max(_maxRad, rad);
+            rads[i] = rad;
+        }
+
+        if (depth == 9)
+        {
+            ConvertVector(x, &_x, &_len);
+            ConvertVector(y, &_y, &_len);
+            _indices = indices;
+            _isLeaf = true;
+        }
+        else
+        {
+            TDoubles rads2(rads);
+            std::sort(rads2.begin(), rads2.end());
+            _rad = rads2[rads2.size()/2];
+
+            TDoubles xInner;
+            TDoubles yInner;
+            TIntVector indicesInner;
+            TDoubles xOuter;
+            TDoubles yOuter;
+            TIntVector indicesOuter;
+
+            for (size_t i = 0; i < x.size(); ++i)
+            {
+                if (rads[i] < _rad)
+                {
+                    xInner.push_back(x[i]);
+                    yInner.push_back(y[i]);
+                    indicesInner.push_back(indices[i]);
+                }
+                else
+                {
+                    xOuter.push_back(x[i]);
+                    yOuter.push_back(y[i]);
+                    indicesOuter.push_back(indices[i]);
+                }
+            }
+
+            if (xInner.size())
+            {
+                _inner = new CircleTree(depth + 1, xInner, yInner, indicesInner);
+            }
+            if (xOuter.size())
+            {
+                _outer = new CircleTree(depth + 1, xOuter, yOuter, indicesOuter);
+            }
+
+            _isLeaf = false;
+        }
+    }
+
+#ifndef _MSC_VER
+    __attribute__((force_align_arg_pointer))
+#endif
+    void Solve(double x, double y, const TVector& x2, const TVector& y2, double* minDist, TVector* minMax, double* minMin, TIntVector* result) const
+    {
+        static const double EPS = 1e-13;
+        const double dQP2 = Sqr(x - _cx) + Sqr(y - _cy);
+        if (_isLeaf)
+        {
+            if (!_len)
+            {
+                return;
+            }
+
+            if ( dQP2 > Sqr(*minDist + _maxRad + EPS) )
+            {
+                return;
+            }
+
+            for (int j = 0; j < _len; ++j)
+            {
+                TVector dx2;
+                dx2.v = _mm_sub_pd(_x[j].v, x2.v);
+                dx2.v = _mm_mul_pd(dx2.v, dx2.v);
+                TVector dy2;
+                dy2.v = _mm_sub_pd(_y[j].v, y2.v);
+                dy2.v = _mm_mul_pd(dy2.v, dy2.v);
+                dx2.v = _mm_add_pd(dx2.v, dy2.v);
+                dx2.v = _mm_sqrt_pd(dx2.v);
+
+                TVector cmpMax;
+                cmpMax.v = _mm_cmplt_pd(dx2.v, minMax->v);
+                for (int k = 0; k < 2; ++k)
+                {
+                    if (cmpMax.d[k])
+                    {
+                        if (dx2.d[k] < minMax->d[0])
+                        {
+                            if (dx2.d[k] < *minDist)
+                            {
+                                if (dx2.d[k] < *minMin)
+                                {
+                                    result->clear();
+                                }
+                                *minDist = dx2.d[k];
+                                minMax->v = _mm_set1_pd(*minDist + EPS);
+                                *minMin = *minDist - EPS;
+                            }
+                            result->push_back(_indices[2 * j + k]);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (_inner)
+            {
+                if (sqrt(dQP2) < _rad + *minDist + EPS)
+                {
+                    _inner->Solve(x, y, x2, y2, minDist, minMax, minMin, result);   
+                }
+            }
+            if (_outer)
+            {
+                if (sqrt(dQP2) + EPS > _rad - *minDist)
+                {
+                    _outer->Solve(x, y, x2, y2, minDist, minMax, minMin, result);
+                }
+            }
+        }
+    }
+};
 void GenBig()
 {
     FILE* fOut = fopen("big.txt", "w");
@@ -327,12 +485,32 @@ void GenBig()
     fclose(fOut);
 }
 
+void GenInt()
+{
+    FILE* fOut = fopen("int.txt", "w");
+    static const int N = 10000;
+    fprintf(fOut, "%d\n", N);
+    for (int i = 0; i < N; ++i)
+    {
+        fprintf(fOut, "%d %d\n", rand() % 10, rand() % 10);
+    }
+    static const int M = 10000;
+    fprintf(fOut, "%d\n", M);
+    for (int i = 0; i < M; ++i)
+    {
+        fprintf(fOut, "%Lf %Lf\n", rand() % 10, rand() % 10);
+    }
+    fclose(fOut);
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     // GenBig();
-    freopen("big.txt", "r", stdin);
+    // freopen("big.txt", "r", stdin);
     // freopen("input.txt", "r", stdin);
+    GenInt();
+    freopen("int.txt", "r", stdin);
 #endif
 
 #ifndef __linux__
@@ -373,7 +551,8 @@ int main()
         yc[i] = ycd[indices[i]] / mx;
     }
 
-    KDTree* kdTree = new KDTree(0, xc, yc, indices);
+    // KDTree* kdTree = new KDTree(0, xc, yc, indices);
+    CircleTree* cTree = new CircleTree(0, xc, yc, indices);
 
     int n;
     scanf("%d", &n);
@@ -398,7 +577,8 @@ int main()
         TVector minMax;
         minMax.v = _mm_set1_pd(minDist);
         double minMin = minDist;
-        kdTree->Solve(x, y, x2, y2, &minDist, &minMax, &minMin, &result);
+        // kdTree->Solve(x, y, x2, y2, &minDist, &minMax, &minMin, &result);
+        cTree->Solve(x, y, x2, y2, &minDist, &minMax, &minMin, &result);
 
         if (result.size() < 10)
         {
