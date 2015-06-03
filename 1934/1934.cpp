0fd3873622cb62259a9afd7bf3180a664a0ddecd
Den Raskovalov <denbox@gmail.com>
2015-06-02 21:55:15 -0700

    1934 AC

--- 1934/1934.cpp
+++ 1934/1934.cpp
@@ -1,20 +1,27 @@
+#ifndef _MSC_VER
+#   pragma GCC target("sse4.2")
+#   pragma GCC optimize("O3")
+#endif
+
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <cstdio>
 #include <cmath>
 
 #include <vector>
 #include <queue>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
 struct TNode {
 	int _to;
-	double _value;
-	double _p;
+    double _p;
 
-	TNode(int to, double value, double p)
+    TNode(int to, double p)
 		: _to(to)
-		, _value(value)
-		, _p(p)
+        , _p(p)
 	{
 
 	}
@@ -24,11 +31,11 @@ struct TNode {
 };
 
 struct TCost {
-	int _iDist;
-	double _fine;
+    int _iDist;
+    double _fine;
 
-	TCost(int iDist, double fine)
-		: _iDist(iDist)
+    TCost(int iDist, double fine)
+        : _iDist(iDist)
 		, _fine(fine)
 	{
 
@@ -38,60 +45,106 @@ struct TCost {
 	{
 
 	}
+
+    bool operator<(const TCost& rhs) const {
+        if (_iDist != rhs._iDist) {
+            return _iDist < rhs._iDist;
+        } else {
+            return _fine > rhs._fine;
+        }
+    }
 };
 
 typedef vector<TNode> TNodes;
 typedef vector<TNodes> TGraph;
 typedef vector<int> TIntegers;
-typedef vector<double> TDoubles;
 typedef queue<int> TQueue;
+typedef vector<TCost> TCosts;
+
+int ReadInt() {
+    int result = 0;
+    char ch = getchar();
+    while (!isdigit(ch))
+        ch = getchar();
+    while (isdigit(ch)) {
+        result = 10 * result + ch - '0';
+        ch = getchar();
+    }
+    return result;
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 #endif
 
-	int n;
-	int m;
-	scanf("%d%%d", &n, &m);
+	int n = ReadInt();
+	int m = ReadInt();
 
-	int s;
-	int f;
-	--s;
-	--f;
+	int s = ReadInt() - 1;
+	int f = ReadInt() - 1;
 
 	TGraph g(n);
 	for (int i = 0; i < m; ++i) {
-		int begin;
-		int end;
-		int cost;
-		scanf("%d%d%d", &begin, &end, &cost);
-		double dCost = log(1.0 - static_cast<double>(cost)/100.0);
-		g[begin].push_back( TNode(end, dCost) );
-		g[end].push_back(TNode(begin, dCost));
-		--begin;
-		--end;
+		int begin = ReadInt() - 1;
+		int end = ReadInt() - 1;
+		int cost = ReadInt();
+
+        double p = 1.0 - static_cast<double>(cost) / 100.0;
+		g[begin].push_back( TNode(end, p) );
+		g[end].push_back( TNode(begin, p) );
 	}
 
-	TIntegers dist(-1, n);
-	TDoubles ddist(0.0, n);
-	TIntegers parent(-1, n);
-	dist[s] = 0;
-	ddist[s] = 0.0;
-	TQueue q;
-	q.push(s);
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
-		for (int i = 0; i < g[now].size(); ++i) {
-			int next = g[now][i]._to;
-			if (-1 == dist[next]) {
-				q.push(next);
-				dist[next] = dist[now] + 1;
-				ddist[next] = d
-			}
-		}
-	}
+    /*
+    for (int i = 0; i < n; ++i) {
+        shuffle(g[i].begin(), g[i].end(), default_random_engine());
+    }
+    */
+
+    const TCost INF(n + 1000, -10.0);
+    TCosts costs(n, INF);
+    TIntegers parent(n, -1);
+    parent[s] = -2;
+
+    TQueue q;
+    {
+        const TCost cost(0, 1.0);
+        q.push(s);
+        costs[s] = cost;
+    }
+
+    while (!q.empty()) {
+        const int now = q.front();
+        q.pop();
+        if (now == f) {
+            break;
+        }
+        const TCost& nowCost = costs[now];
+        for (TNodes::const_iterator toNode = g[now].begin(); toNode != g[now].end(); ++toNode) {
+            const int next = toNode->_to;
+            TCost nextCost(nowCost._iDist + 1, nowCost._fine*toNode->_p);
+            if (-1 == parent[next] || ((nowCost._iDist + 1 == costs[next]._iDist) && (nowCost._fine*toNode->_p > costs[next]._fine))) {
+                if (-1 == parent[next]) {
+                    q.push(next);
+                }
+                costs[next] = nextCost;
+                parent[next] = now;
+            }
+        }
+    }
+
+    TIntegers path;
+    int now = f;
+    while (-2 != now) {
+        path.push_back(now);
+        now = parent[now];
+    }
+
+    printf("%d %.12lf\n", path.size(), 1.0 - costs[f]._fine);
+    for (TIntegers::const_reverse_iterator toPath = path.rbegin(); toPath != path.rend(); ++toPath) {
+        printf("%d ", *toPath + 1);
+    }
+    printf("\n");
 
 	return 0;
 }
