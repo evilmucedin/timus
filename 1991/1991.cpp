a5dac6c9b58c169d77b6ecbc59b5d1f7d3a59560
Den Raskovalov <denbox@gmail.com>
2015-06-08 19:25:48 -0700

    optmization

--- 1991/1991.cpp
+++ 1991/1991.cpp
@@ -1,16 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <cstdio>
+#include <cctype>
+
+int ReadInt() {
+    int result = 0;
+    char ch = getchar();
+    while (!isdigit(ch))
+        ch = getchar();
+    while (isdigit(ch)) {
+        result = 10 * result + ch - '0';
+        ch = getchar();
+    }
+    return result;
+}
 
 int main() {
-    int n;
-    int k;
-    scanf("%d%d", &n, &k);
+    int n = ReadInt();
+    int k = ReadInt();
     int remBB = 0;
     int remDroids = 0;
     for (int i = 0; i < n; ++i) {
-        int bb;
-        scanf("%d", &bb);
+        int bb = ReadInt();
         if (bb > k) {
             remBB += bb - k;
         } else {
